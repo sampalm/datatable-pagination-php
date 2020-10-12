94b03a525ceb5ceb088c4e1b08e7380048b568ff
sampalm <samuelpalmeira@outlook.com>
2020-10-11 22:33:44 -0300

    also adds bootstrap

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
     <title>Datatable Example</title>
     <!-- Datatable CSS -->
     <link href='//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css' rel='stylesheet' type='text/css'>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -17,20 +22,25 @@
     <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 
     <!-- Table -->
-    <table id='empTable' class='display dataTable'>
+    <div class="content">
+        <div class="row align-items-center center">
+            <div class="col col-sm-8 mx-auto">
+                <table id='empTable' class='display dataTable table'>
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>ID</th>
+                            <th>Autor</th>
+                            <th>Titulo</th>
+                            <th>Descrição</th>
+                            <th>Conteudo</th>
+                            <th>Data</th>
+                        </tr>
+                    </thead>
 
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Autor</th>
-                <th>Titulo</th>
-                <th>Descrição</th>
-                <th>Conteudo</th>
-                <th>Data</th>
-            </tr>
-        </thead>
-
-    </table>
+                </table>
+            </div>
+        </div>
+    </div>
 
     <script>
         $(document).ready(function() {
