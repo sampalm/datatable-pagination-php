b5210550b01f25f22419be0d2ad1605293c7c30e
sampalm <samuelpalmeira@outlook.com>
2020-10-12 00:03:30 -0300

    customize datatable && changes datatable settings

--- index.php
+++ index.php
@@ -6,12 +6,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Datatable Example</title>
     <!-- Datatable CSS -->
-    <link href='//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css' rel='stylesheet' type='text/css'>
+    <link href='datatables/datatables.min.css' rel='stylesheet' type='text/css'>
+    <link href='datatables/datatables.bootstrap4.min.css' rel='stylesheet' type='text/css'>
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+
+    <style>
+        #empTable_wrapper > div.row > div.col-md-6:nth-child(1){
+            display: none;
+        }
+        #empTable_wrapper > div.row > div.col-md-6:nth-child(3) label {
+            padding: 6px;
+            margin-left: 10px;
+        }
+        #empTable_filter label {
+            width: 100%;
+        }
+
+        #empTable_filter input {
+            width: 450px;
+            margin: 5px;
+            padding-left: 50px;
+        }
+        
+    </style>
 </head>
 
 <body>
@@ -19,14 +42,14 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
     <!-- Datatable JS -->
-    <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script src="datatables/datatables.min.js"></script>
 
     <!-- Table -->
     <div class="content">
         <div class="row align-items-center center">
             <div class="col col-sm-8 mx-auto">
-                <table id='empTable' class='display dataTable table'>
-                    <thead class="thead-dark">
+                <table id='empTable' class="table table-striped table-bordered" style="width:100%">
+                    <thead>
                         <tr>
                             <th>ID</th>
                             <th>Autor</th>
@@ -48,9 +71,15 @@
                 'processing': true,
                 'serverSide': true,
                 'serverMethod': 'post',
+                "lengthChange": false,
                 'ajax': {
                     'url': 'ajax.php'
                 },
+                "language": {
+                    "search": "",
+                    "searchPlaceholder": "Pesquisar por.."
+                },
+                //"bFilter": false, Remove search box
                 'columns': [{
                         data: 'id'
                     },
@@ -70,6 +99,18 @@
                         data: 'date'
                     }
                 ]
+            });
+            $('#empTable').ready(()=>{
+                var filter = $('<div class="col-sm-12 col-md-6">');
+                filter.html(`
+                    <label>Autor
+                        <input type="radio" id="author" name="filtro" value="author">
+                    </label>
+                    <label>Titulo
+                        <input type="radio" id="title" name="filtro" value="title">
+                    </label>
+                `)
+                .insertAfter('#empTable_wrapper div.col-md-6:nth-child(2)');
             });
         });
     </script>
